ece5d3064d68c1daa67d2a717aa8124dfe0ec225
tadamiura <tadamiura@gmail.com>
2020-04-27 21:07:27 +0200

    Use toUpperCase to transform a str to uppercase and create my own test

--- exercises/yell.js
+++ exercises/yell.js
@@ -7,9 +7,20 @@
  */
 
 // Your code:
-
+const yell = (str) => {
+    str.toUpperCase()
+}
+ return yell
 //* Begin of tests
 const assert = require('assert');
+
+assert.strictEqual(typeof sentence, 'function');
+assert.strictEqual(sentence.length, 1);
+assert.deepStrictEqual(sentence('abc'), 'ABC');
+assert.deepStrictEqual(sentence('Hello World'), 'HELLO WORLD');
+assert.deepStrictEqual(sentence('UPPERCASE'), 'UPPERCASE');
+assert.deepStrictEqual(sentence('1'), '1');
+
 
 assert.fail('You must write your own tests');
 // End of tests */
